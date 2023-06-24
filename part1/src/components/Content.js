47dbec1f08ada912038f6b6135daa19abf4b3872
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2023-06-24 21:25:32 +0800

    Exercises 1.1, 1.2

---
+++ part1/src/components/Content.js
@@ -0,0 +1,19 @@
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  )
+}
+
+export default Content
