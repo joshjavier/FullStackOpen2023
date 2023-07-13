31d1039776caadb08e2de0cd42e9a6178191d3de
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2023-07-14 07:23:08 +0800

    Exercise 3.3

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -39,6 +39,17 @@ app.get('/api/info', (request, response) => {
   response.send(`<p>${info}</p><p>${now}</p>`)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find((person) => person.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
