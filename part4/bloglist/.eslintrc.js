f1a977a8adf2baae54cf81cd5c78d5fd532bb217
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2023-07-17 08:50:54 +0800

    Exercise 4.3

---
+++ part4/bloglist/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: ['eslint:recommended', 'prettier'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {},
+}
