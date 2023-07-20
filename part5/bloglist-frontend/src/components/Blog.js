681d55396d46a18df604f21ad346c26d77719419
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2023-07-21 05:38:00 +0800

    Exercise 5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,32 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog: { title, author, url, likes, user } }) => {
+  const [hidden, setHidden] = useState(true)
+
+  const toggle = () => {
+    setHidden(!hidden)
+  }
+
+  return (
+    <div className="blog-item">
+      <div>
+        <span>
+          {title}
+          {author && ` by ${author}`}
+        </span>{' '}
+        <button onClick={toggle}>{hidden ? 'view' : 'hide'}</button>
+      </div>
+      <div hidden={hidden} className="details">
+        <p>
+          <a href={url}>🔗 {url}</a>
+        </p>
+        <p>
+          <span>❤️ likes: {likes}</span> <button>like</button>
+        </p>
+        {user && <p>👤 {user?.name || user.username}</p>}
+      </div>
+    </div>
+  )
+}
 
 export default Blog
